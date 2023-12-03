50e4c4beec69903b53827d6f51fdfa954076ab3a
SunilCz <86451565+SunilCz@users.noreply.github.com>
2023-12-03 14:11:05 +0545

    Update socketController.js

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,18 +1,23 @@
-// socketController.js
 const Room = require("../models/roomModel");
 
-// Maintain a mapping of socket.id to userId
+// Maintain a mapping of socket.id to userId and room associations
 const socketIdToUserIdMap = {};
+const socketIdToRoomMap = {};
 
 const handleConnection = (socket, io) => {
   console.log("Connected & Socket Id is ", socket.id);
   socket.emit("Data", "first emit");
 
-  // Receive the userId as part of the connection handshake
-  socket.on("connectUser", (userId) => {
-    // Associate the userId with the socket connection
+  // Receive the userId and roomId as part of the connection handshake
+  socket.on("connectUser", ({ userId, roomId }) => {
+    // Associate the userId and roomId with the socket connection
     socketIdToUserIdMap[socket.id] = userId;
-    console.log(`User with ID ${userId} connected.`);
+    socketIdToRoomMap[socket.id] = roomId;
+    
+    // Join the room
+    socket.join(roomId);
+
+    console.log(`User with ID ${userId} connected to room ${roomId}.`);
 
     // You might emit additional events or perform other actions here if needed.
   });
@@ -25,9 +30,9 @@ const handleConnection = (socket, io) => {
     try {
       console.log("Location data received:", data);
 
-      // Broadcast the location data to all connected clients
-      io.emit("LocationUpdate", data);
-      console.log('Location data emitted to all connected clients.', data);
+      // Broadcast the location data to all clients in the same room
+      io.to(socketIdToRoomMap[socket.id]).emit("LocationUpdate", data);
+      console.log('Location data emitted to all clients in the same room.', data);
 
     } catch (error) {
       console.error('Error handling location data:', error.message);
@@ -37,8 +42,15 @@ const handleConnection = (socket, io) => {
   // Handle disconnection and remove the mapping
   socket.on("disconnect", () => {
     const userId = socketIdToUserIdMap[socket.id];
+    const roomId = socketIdToRoomMap[socket.id];
+    
     delete socketIdToUserIdMap[socket.id];
-    console.log(`User with ID ${userId} disconnected.`);
+    delete socketIdToRoomMap[socket.id];
+
+    // Leave the room
+    socket.leave(roomId);
+
+    console.log(`User with ID ${userId} disconnected from room ${roomId}.`);
   });
 }
 
