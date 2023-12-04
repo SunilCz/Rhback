2a36ca3a3493d017cc4b9601020a3195b7324cfa
SunilCz <86451565+SunilCz@users.noreply.github.com>
2023-12-04 09:12:42 +0545

    Update socketController.js

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,6 +1,10 @@
 // socketController.js
 const Room = require("../models/roomModel");
 
+// Store shared location data
+const sharedLocations = {};
+
+
 const handleConnection = (socket, io) => {
   console.log("Connected & Socket Id is ", socket.id);
   socket.emit("Data", "first emit");
@@ -9,19 +13,41 @@ const handleConnection = (socket, io) => {
     console.log(data);
   });
 
-  socket.on("TrackLocation", (data) => {
+  socket.on("ShareLocation", (data) => {
     try {
       console.log("Location data received:", data);
-  
+
+      // Store the shared location data
+      sharedLocations[data.userId] = data;
+
       // Broadcast the location data to all connected clients
-      io.emit("LocationUpdate", data);
+      io.emit("SharedLocation", data);
       console.log('Location data emitted to all connected clients.', data);
-  
+
     } catch (error) {
       console.error('Error handling location data:', error.message);
     }
   });
-}  
+
+  socket.on("TrackLocation", (userId) => {
+    try {
+      // Retrieve the shared location data for the specified user
+      const locationData = sharedLocations[userId];
+
+      if (locationData) {
+        // Send the location data to the tracking client
+        socket.emit("TrackedLocation", locationData);
+        console.log('Tracked location data sent to the client.', locationData);
+      } else {
+        console.log('Location data not found for user:', userId);
+      }
+
+    } catch (error) {
+      console.error('Error handling track location request:', error.message);
+    }
+  });
+};
+
 module.exports = {
   handleConnection,
 };
