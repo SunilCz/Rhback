fff7cea2bbd22ba2f63618f77ba3b1b9c842622b
SunilCz <86451565+SunilCz@users.noreply.github.com>
2023-12-04 19:13:56 +0545

    Update server.js

--- server.js
+++ server.js
@@ -8,44 +8,41 @@ const userRoute = require('./routes/userRoute');
 const errorHandler = require('./middleware/errorMiddleware');
 const http = require('http');
 const socketIO = require('socket.io');
-const socketController = require('./controllers/socketController');
-const socketRoutes = require('./routes/socketRoutes');
-
+const socketController = require('./controllers/socketController'); // Import socketController
+const socketRoutes = require('./routes/socketRoutes'); // Import socketRoutes
 const app = express();
 const httpServer = http.createServer(app);
 
-const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:3000,https://meek-babka-71c876.netlify.app/,http://192.168.1.6:3000';
-
+// Use the same http server instance for Socket.IO
 const io = new socketIO.Server(httpServer, {
   cors: {
-    origin: corsOrigin,
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000' || 'https://meek-babka-71c876.netlify.app/' || 'http://192.168.1.6:3000', // Use an environment variable
     methods: ['GET', 'POST'],
   },
 });
-
 io.on('connection', (socket) => {
   socketController.handleConnection(socket, io);
-});
-
+})
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
-
 app.use(
   cors({
-    origin: corsOrigin.split(','),
+    origin: ['http://localhost:3000', 'https://meek-babka-71c876.netlify.app/', 'http://192.168.1.6:3000'],
     credentials: true,
   })
 );
 
+// Routes
 app.use('/api/users', userRoute);
 app.use("/socket", socketRoutes);
-
 app.get('/', (req, res) => {
   res.send('Home Page');
 });
 
+// Error Handler
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
@@ -56,4 +53,5 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
   });
 });
 
-exports.io = io;
+
+exports.io = io; // Export the Socket.IO instance so it can be used in socketRoutes.js
